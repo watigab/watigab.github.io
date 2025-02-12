6202cb69edfa11f29ff3e8e9b1140b337b7d76a6
watigab <gabriel.languille@viacesi.fr>
2025-02-12 19:44:58 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -21,8 +21,8 @@ import { remarkReadingTime } from './src/plugins/remark-reading-time.mjs'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://watigab.github.io/',
-  base: '/',
+  site: 'https://watigab.github.io',
+  //base: '/',
   trailingSlash: 'always',
   integrations: [
     tailwind({
