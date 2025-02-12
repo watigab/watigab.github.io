ba3f916b14590772739e69711c21fb03ecd85d37
watigab <gabriel.languille@viacesi.fr>
2025-02-12 20:36:34 +0100

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -9,7 +9,7 @@ import { LinkPreset } from './types/config'
 export const siteConfig: SiteConfig = {
   title: 'c mon site',
   subtitle: 'jsp',
-  lang: 'en',         // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko', 'es', 'th'
+  lang: 'fr',         // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko', 'es', 'th'
   themeColor: {
     hue: 250,         // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
     fixed: false,     // Hide the theme color picker for visitors
@@ -52,7 +52,7 @@ export const navBarConfig: NavBarConfig = {
 
 export const profileConfig: ProfileConfig = {
   avatar: 'assets/images/BMO-fart-avatar.gif',  // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-  name: 'slt',
+  name: 'gab',
   bio: 'en fait c pcq tu captes',
   links: [
     {
